418798ebf89177b433c0bf7e908700c2d8927d12
Baptiste Fontaine <batifon@yahoo.fr>
2013-09-23 22:46:08 +0200

    Basic formatting & across-sessions saving.

--- preview.js
+++ preview.js
@@ -2,7 +2,18 @@
 
     var inp = document.getElementById('pastek-input'),
         out = document.getElementById('html-output'),
-        warn = window.console && console.warn ? console.warn.bind(console) : function(){};
+        warn = window.console && console.warn ? console.warn.bind(console) : function(){},
+        s;
+
+    function addEventListener( el, name, fn ) {
+        if  (el.addEventListener) { // Modern browsers
+            el.addEventListener(name, fn, false);
+        } else if (inp.attachEvent) { // IE
+            inp.attachEvent('on' + name, fn);
+        } else { // others
+            inp['on' + name] = fn;
+        }
+    }
 
     function updateHTML() {
         var text = inp.value;
@@ -14,13 +25,20 @@
         }
     }
 
-    if  (inp.addEventListener) { // Modern browsers
-        inp.addEventListener('keyup', updateHTML, false);
-    } else if (inp.attachEvent) { // IE
-        inp.attachEvent('onkeyup', updateHTML);
-    } else { // others
-        inp.onkeyup = updateHTML;
-    }
+    addEventListener(inp, 'keyup', updateHTML);
 
+    // save text across sessions
+    if (window.localStorage && typeof localStorage.getItem == 'function') {
+        s = localStorage.getItem('pastek.text');
+
+        if (s && inp.value.length == 0) {
+            inp.value = s;
+            updateHTML();
+        }
+
+        addEventListener(window, 'unload', function() {
+            localStorage.setItem('pastek.text', inp.value);
+        });
+    }
 
 })();
