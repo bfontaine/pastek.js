f01344f0ccd09d8c334072c5d571dc7982f550cf
Baptiste Fontaine <batifon@yahoo.fr>
2013-09-23 22:18:53 +0200

    Basic preview app added.

---
+++ preview.js
@@ -0,0 +1,26 @@
+(function() {
+
+    var inp = document.getElementById('pastek-input'),
+        out = document.getElementById('html-output'),
+        warn = window.console && console.warn ? console.warn.bind(console) : function(){};
+
+    function updateHTML() {
+        var text = inp.value;
+
+        try {
+            out.innerHTML = pastek_core.mk_html(text + "\n");
+        } catch(e) {
+            warn('' + e + ', text=<'+text.replace('\n', '\\n')+'>');
+        }
+    }
+
+    if  (inp.addEventListener) { // Modern browsers
+        inp.addEventListener('keyup', updateHTML, false);
+    } else if (inp.attachEvent) { // IE
+        inp.attachEvent('onkeyup', updateHTML);
+    } else { // others
+        inp.onkeyup = updateHTML;
+    }
+
+
+})();
