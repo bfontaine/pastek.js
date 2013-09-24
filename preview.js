96402afca68c9c19fbde462174f5c61df2ee9e74
Baptiste Fontaine <batifon@yahoo.fr>
2013-09-24 15:05:26 +0200

    'view raw' & 'save as' buttons added.

--- preview.js
+++ preview.js
@@ -1,16 +1,19 @@
 (function() {
 
-    var inp = document.getElementById('pastek-input'),
-        out = document.getElementById('html-output'),
+    var inp   = document.getElementById('pastek-input'),
+        out   = document.getElementById('html-output'),
+        tools = document.getElementById('input-tools'),
+        rawbutton = document.createElement('a'),
+        savebutton,
         cache = '',
         warn = window.console && console.warn ? console.warn.bind(console) : function(){},
         empty_re = /^\s*$/,
         help_text = document.getElementById('intro-text').innerHTML,
         s;
 
-    function addEventListener( el, name, fn ) {
+    function addEventListener( el, name, fn, useCapture ) {
         if  (el.addEventListener) { // Modern browsers
-            el.addEventListener(name, fn, false);
+            el.addEventListener(name, fn, !!useCapture);
         } else if (inp.attachEvent) { // IE
             inp.attachEvent('on' + name, fn);
         } else { // others
@@ -18,6 +21,10 @@
         }
     }
 
+    /**
+     * Main rendering function. It reads the Pastek input,
+     * and update the HTML one.
+     **/
     function updateHTML() {
         var text = inp.value.replace(/\n+$/g, '');
 
@@ -39,7 +46,10 @@
 
     addEventListener(inp, 'keyup', updateHTML);
 
-    // save text across sessions
+    /**
+     * Save text across sessions
+     **/
+
     if (window.localStorage && typeof localStorage.getItem == 'function') {
         s = localStorage.getItem('pastek.text');
 
@@ -54,11 +64,51 @@
         });
     }
 
-    // intro text
+    /**
+     * On startup include the help text if the input is empty
+     **/
+
     if (inp.value.length == 0) {
 
         inp.value = help_text;
         updateHTML();
+    }
+
+    /**
+     * Save Pastek input as a file using a Blob, if it's supported
+     * by the browser.
+     **/
+
+    function createSourceURL() {
+        var b = new Blob([inp.value], {type: "text/plain"});
+        return URL.createObjectURL(b);
+    }
+
+    if (window.Blob && window.URL && typeof URL.createObjectURL == 'function') {
+
+        rawbutton.className = 'button';
+        rawbutton.textContent = rawbutton.innerText = 'View raw';
+        rawbutton.setAttribute('href', '#');
+        rawbutton.setAttribute('target', '_blank');
+        rawbutton.setAttribute('title', 'Right-click → "Save as…"');
+
+        savebutton = rawbutton.cloneNode(true);
+        savebutton.textContent = savebutton.innerText = 'Save as…';
+        savebutton.setAttribute('download', 'pastek.txt');
+
+        addEventListener(rawbutton, 'click', function() {
+            rawbutton.setAttribute('href', createSourceURL());
+        });
+
+        addEventListener(savebutton, 'click', function() {
+            savebutton.setAttribute('href', createSourceURL());
+            savebutton.setAttribute('download',
+                                    prompt('Select a filename:', 'pastek.txt'));
+        });
+
+        tools.appendChild(rawbutton);
+        tools.appendChild(savebutton);
+
     }
 
 })();
