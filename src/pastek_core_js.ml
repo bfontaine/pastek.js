88d98a72b15f8a604126895a2f12e0481d444c86
Julien Sagot <ju.sagot@gmail.com>
2013-10-14 14:09:39 +0200

    upgraded with last version

--- src/pastek_core_js.ml
+++ src/pastek_core_js.ml
@@ -23,9 +23,15 @@
 open Parser
 open Lexer
 open Html_of_core
+open Pastek_core
 
 let mk_html_of_js_string s =
-  let ast = Parser.document Lexer.main (Lexing.from_string (Js.to_string s))
-  in Js.string (Html_of_core.html_of_ast ast)
+  let lexbuf = (Lexing.from_string (Js.to_string s))
+  in
+  Js.string (html_of_ast (parse_input lexbuf))
 
-let _ = Js.Unsafe.set (Js.Unsafe.variable "pastek_core") (Js.string "mk_html") (Js.wrap_callback mk_html_of_js_string)
+let _ =
+  Js.Unsafe.set
+    (Js.Unsafe.variable "pastek_core")
+    (Js.string "mk_html")
+    (Js.wrap_callback mk_html_of_js_string)
